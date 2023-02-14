dab47f85dbcaac2fec6c7c616d0d77152c50a3e2
Andrej Jasso <andrej.jasso@goodrequest.com>
2023-02-14 20:32:27 +0100

    build: Fixed compilation errors

--- Package.swift
+++ Package.swift
@@ -29,7 +29,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GoodNetworking",
-            dependencies: ["Alamofire", "AlamofireImage"],
+            dependencies: [
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "AlamofireImage", package: "AlamofireImage")
+            ],
             path: "./Sources/GoodNetworking"
         ),
         .target(
