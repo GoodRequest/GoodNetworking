b80f1db3ce52266f2c2b95247feaf7be48310962
Matus Mistrik <matus.mistrik@goodrequest.com>
2024-03-15 13:26:53 +0100

    feat: Alamofire update to 5.9.0

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.0")),
         .package(url: "https://github.com/Alamofire/AlamofireImage.git", .upToNextMajor(from: "4.2.0"))
     ],
     targets: [
