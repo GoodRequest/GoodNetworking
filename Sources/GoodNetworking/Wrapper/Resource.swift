a7125ff0d2045fdd368598bd5773f8486de75193
Filip Sasala <31418257+plajdo@users.noreply.github.com>
2024-11-12 13:28:29 +0100

    Fix NetworkError handling

--- Sources/GoodNetworking/Wrapper/Resource.swift
+++ Sources/GoodNetworking/Wrapper/Resource.swift
@@ -172,11 +172,11 @@ extension Resource where R: Creatable {
             self.rawResponse.create = response
 
             let resource = try R.resource(from: response, updating: resource)
-            try checkCancellation()
+            try Task.checkCancellation()
 
             self.state = .available(resource)
             self.listState = .available([resource])
-        } catch let error {
+        } catch let error as NetworkError {
             if let resource {
                 self.state = .stale(resource, error)
                 self.listState = .stale([resource], error)
@@ -185,6 +185,8 @@ extension Resource where R: Creatable {
                 self.listState = .failure(error)
             }
 
+            throw error
+        } catch {
             throw error
         }
     }
@@ -224,11 +226,11 @@ extension Resource where R: Readable {
             self.rawResponse.read = response
 
             let resource = try R.resource(from: response, updating: resource)
-            try checkCancellation()
+            try Task.checkCancellation()
 
             self.state = .available(resource)
             self.listState = .available([resource])
-        } catch let error {
+        } catch let error as NetworkError {
             if let resource {
                 self.state = .stale(resource, error)
                 self.listState = .stale([resource], error)
@@ -237,6 +239,8 @@ extension Resource where R: Readable {
                 self.listState = .failure(error)
             }
 
+            throw error
+        } catch {
             throw error
         }
     }
@@ -273,11 +277,11 @@ extension Resource where R: Updatable {
             self.rawResponse.update = response
 
             let resource = try R.resource(from: response, updating: resource)
-            try checkCancellation()
+            try Task.checkCancellation()
 
             self.state = .available(resource)
             self.listState = .available([resource])
-        } catch let error {
+        } catch let error as NetworkError {
             if let resource {
                 self.state = .stale(resource, error)
                 self.listState = .stale([resource], error)
@@ -286,6 +290,8 @@ extension Resource where R: Updatable {
                 self.listState = .failure(error)
             }
 
+            throw error
+        } catch {
             throw error
         }
     }
@@ -316,7 +322,7 @@ extension Resource where R: Deletable {
             self.rawResponse.delete = response
 
             let resource = try R.resource(from: response, updating: state.value)
-            try checkCancellation()
+            try Task.checkCancellation()
 
             if let resource {
                 // case with partial/soft delete only
@@ -327,10 +333,12 @@ extension Resource where R: Deletable {
                 #warning("TODO: vymazat z listu iba prave vymazovany element")
                 self.listState = .idle
             }
-        } catch let error {
+        } catch let error as NetworkError {
             self.state = .failure(error)
             self.listState = .failure(error)
 
+            throw error
+        } catch {
             throw error
         }
     }
@@ -414,21 +422,6 @@ extension Resource where R: Listable {
             self.listState = .failure(error)
 
             throw error
-        }
-    }
-
-}
-
-// MARK: - Cancellation handling
-
-@available(iOS 17.0, *)
-extension Resource {
-
-    func checkCancellation() throws(NetworkError) {
-        do {
-            try Task.checkCancellation()
-        } catch {
-            throw NetworkError.cancelled
         }
     }
 
